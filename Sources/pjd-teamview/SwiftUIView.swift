44d02c30da6a6514366ec1d94880ef8f0b1b965b
taimoor <taimoor@pumpjackdataworks.com>
2022-06-23 16:40:12 +0500

    added content view

---
+++ Sources/pjd-teamview/SwiftUIView.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Admin on 23/06/2022.
+//
+
+import SwiftUI
+
+@available(macOS 11.0, *)
+@main
+struct jsonModuleServiceSwiftUIApp: App {
+    @available(macOS 11.0, *)
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+        }
+    }
+}
+
+
+//struct SwiftUIView: View {
+//    @available(macOS 10.15, *)
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
+//
+//struct SwiftUIView_Previews: PreviewProvider {
+//    @available(macOS 10.15, *)
+//    static var previews: some View {
+//        SwiftUIView()
+//    }
+//}
